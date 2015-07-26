ae2d290383a057a83bda649ade94892e1616b043
kzif <ismael_icefenix@hotmail.com>
2015-07-26 16:30:39 -0300

    Update jspdf.js

--- jspdf.js
+++ jspdf.js
@@ -82,6 +82,7 @@ var jsPDF = (function(global) {
 			'junior-legal'      : [576,   360],
 			'ledger'            : [1224,  792],
 			'tabloid'           : [792,  1224],
+			'longer'            : [595,  99999999],
 			'credit-card'       : [153,   243]
 		};
 
