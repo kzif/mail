fd1dbf02d016a3654ef91902c8d0b8010bc85523
kzif <ismael_icefenix@hotmail.com>
2015-07-27 17:46:06 -0300

    Update from_html.js

--- plugin/from_html.js
+++ plugin/from_html.js
@@ -294,6 +294,7 @@
 			rowData = {};
 			j = 0;
 			while (j < tableRow.cells.length) {
+				if(headers[j] === undefined) continue;
 				rowData[headers[j].name] = tableRow.cells[j].textContent.replace(/\r?\n/g, '');
 				j++;
 			}
@@ -945,7 +946,8 @@
 			var indentMove = 0;
 			var indentMore = 0;
 			//if a margin was added (by e.g. a text-alignment), move the cursor
-			console.log(line[0]);
+			console.log(line[0][0]);
+			console.log(line[0][1]);
 			if (line[0][1]["margin-left"] !== undefined && line[0][1]["margin-left"] > 0) {
 				wantedIndent = this.pdf.internal.getCoordinateString(line[0][1]["margin-left"]);
 				indentMove = wantedIndent - currentIndent;
