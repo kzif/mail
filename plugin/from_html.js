5adedbf01293b7ff627fc18e4993ec1cec5738d9
kzif <ismael_icefenix@hotmail.com>
2015-07-28 13:27:07 -0300

    Update from_html.js

--- plugin/from_html.js
+++ plugin/from_html.js
@@ -947,7 +947,6 @@
 			var indentMove = 0;
 			var indentMore = 0;
 			//if a margin was added (by e.g. a text-alignment), move the cursor
-			console.log(line[0]);
 			if (line[0][1]["margin-left"] !== undefined && line[0][1]["margin-left"] > 0) {
 				wantedIndent = this.pdf.internal.getCoordinateString(line[0][1]["margin-left"]);
 				indentMove = wantedIndent - currentIndent;
